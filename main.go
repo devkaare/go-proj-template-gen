f27535e3ba656efe5c35c9e688fe2abe38d83492
devkaare <kaarehofferdal@gmail.com>
2025-02-09 14:39:23 +0100

    Fix

--- main.go
+++ main.go
@@ -38,60 +38,61 @@ var (
 
 func addNameToFiles(name string) (map[string]string, error) {
 	result := make(map[string]string, 50)
-	mainFileData := fmt.Sprintf(`
-		package main
+	mainFileData := fmt.Sprintf(
+		`package main
 
-		import (
-			"context"
-			"fmt"
-			"log"
-			"net/http"
-			"os/signal"
-			"syscall"
-			"time"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
-			"github.com/devkaare/%s/server"
-		)
+	"github.com/devkaare/%s/server"
+)
 
-		func gracefulShutdown(apiServer *http.Server, done chan bool) {
-			ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-			defer stop()
+func gracefulShutdown(apiServer *http.Server, done chan bool) {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
-			<-ctx.Done()
+	<-ctx.Done()
 
-			log.Println("shutting down gracefully, press Ctrl+C again to force")
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
 
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancel()
-			if err := apiServer.Shutdown(ctx); err != nil {
-				log.Printf("Server forced to shutdown with error :%%v", err)
-			}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := apiServer.Shutdown(ctx); err != nil {
+		log.Printf("Server forced to shutdown with error :%%v", err)
+	}
 
-			log.Println("Server exiting")
+	log.Println("Server exiting")
 
-			done <- true
-		}
+	done <- true
+}
 
-		func main() {
-			server := server.NewServer()
+func main() {
+	server := server.NewServer()
 
-			done := make(chan bool, 1)
+	done := make(chan bool, 1)
 
-			go gracefulShutdown(server, done)
+	go gracefulShutdown(server, done)
 
-			err := server.ListenAndServe()
-			if err != nil && err != http.ErrServerClosed {
-				panic(fmt.Sprintf("http server error: %%s", err))
-			}
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		panic(fmt.Sprintf("http server error: %%s", err))
+	}
 
-			<-done
+	<-done
 
-			log.Println("Graceful shutdown complete.")
-		}
+	log.Println("Graceful shutdown complete.")
+}
 	`, name)
 	result[mainFilePath] = mainFileData
 
-	handlerFileData := fmt.Sprintf(`
+	handlerFileData :=
+		`
 		package handler
 
 		import "net/http"
@@ -101,7 +102,7 @@ func addNameToFiles(name string) (map[string]string, error) {
 		func (t *New) Greet(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("Hello World!"))
 		}
-	`, name)
+		`
 	result[handlerFilePath] = handlerFileData
 
 	serverFileData := fmt.Sprintf(`
